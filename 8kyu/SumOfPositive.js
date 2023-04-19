4a122f5da680f1d6d979d41c20db42d68bc4a92a
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2023-04-19 14:08:06 +0530

    refactored code

--- 8kyu/SumOfPositive.js
+++ 8kyu/SumOfPositive.js
@@ -7,8 +7,9 @@
 function positiveSum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) sum += arr[i];
-        else sum += 0;
+        if (arr[i] > 0) {
+            sum += arr[i];
+        }
     }
     return sum;
 }
