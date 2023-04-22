d50a9052a8935607af7d5d226894b99b7205aae4
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2023-04-22 19:31:40 +0530

    refactored code + added problem link

--- 7kyu/VowelCount.js
+++ 7kyu/VowelCount.js
@@ -1,20 +1,18 @@
-// Return the number (count) of vowels in the given string.
+/*  Problem Link: https://www.codewars.com/kata/54ff3102c1bad923760001f3 
 
-// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+Return the number (count) of vowels in the given string.
 
-// The input string will only consist of lower case letters and/or spaces.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces. */
 
 function getCount(str) {
+    const vowels = ["a", "e", "i", "o", "u"];
     let count = 0;
     for (let i = 0; i < str.length; i++) {
-        if (
-            str[i] === "a" ||
-            str[i] === "e" ||
-            str[i] === "i" ||
-            str[i] === "o" ||
-            str[i] === "u"
-        )
+        if (vowels.includes(str[i]) === true) {
             count++;
+        }
     }
     return count;
 }
