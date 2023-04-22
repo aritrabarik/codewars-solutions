2bd757b68940163e80c3cc0c30bbbea68caa5103
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2023-04-22 18:10:23 +0530

    refactored code to follow best practice

--- 7kyu/DisemvowelTrolls.js
+++ 7kyu/DisemvowelTrolls.js
@@ -1,30 +1,20 @@
-// Trolls are attacking your comment section!
+/* Trolls are attacking your comment section!
 
-// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
 
-// Your task is to write a function that takes a string and return a new string with all vowels removed.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
 
-// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
-// Note: for this kata y isn't considered a vowel.
+Note: for this kata y isn't considered a vowel. */
 
 function disemvowel(str) {
     let newStr = "";
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     for (let i = 0; i < str.length; i++) {
-        if (
-            str[i] === "a" ||
-            str[i] === "A" ||
-            str[i] === "e" ||
-            str[i] === "E" ||
-            str[i] === "i" ||
-            str[i] === "I" ||
-            str[i] === "o" ||
-            str[i] === "O" ||
-            str[i] === "u" ||
-            str[i] === "U"
-        )
-            newStr += "";
-        else newStr += str[i];
+        if (vowels.includes(str[i]) === false) {
+            newStr += str[i];
+        }
     }
     return newStr;
 }
