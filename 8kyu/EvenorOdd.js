295fe627e12939f8fd7455a4b87f254594c15f2e
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2023-04-23 18:47:33 +0530

    added problem link

--- 8kyu/EvenorOdd.js
+++ 8kyu/EvenorOdd.js
@@ -1,6 +1,6 @@
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+/*  Problem Link: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
 
-// Solution:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
 
 function even_or_odd(number) {
     return number % 2 === 0 ? "Even" : "Odd";
--- 8kyu/SumOfPositive.js
+++ 8kyu/SumOfPositive.js
@@ -1,8 +1,10 @@
-// You get an array of numbers, return the sum of all of the positives ones.
+/*  Problem Link: https://www.codewars.com/kata/5715eaedb436cf5606000381 
 
-// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+You get an array of numbers, return the sum of all of the positives ones.
 
-// Note: if there is nothing to sum, the sum is default to 0.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0. */
 
 function positiveSum(arr) {
     let sum = 0;
